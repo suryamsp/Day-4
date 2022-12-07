b574e7a72dd77a857083eb67f920fe4038669825
suryamsp <118907217+suryamsp@users.noreply.github.com>
2022-12-07 18:21:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,15 @@
 
 var obj1 = {
     name:"Person 1",
-    age:5
+    age:"5"
 }
 var obj2 = {
-    age:5,
-    name:"Person 1",
+    age:"5",
+    name:"Person 1"
     
 }
 
-var equal= function(){
-    k1=Object.keys(obj1);
-    k2=Object.keys(obj2);
-    return k1.length===k2.length && Object.keys(obj1).every(key=>obj1[key]==obj2[key]);
-    
-}
-console.log("JSON Object Equal is:  "+equal(obj1,obj2));
-
-
+console.log(JSON.stringify({obj1})==JSON.stringify({obj2}));
 
 // print all country flags
 var store= new XMLHttpRequest();
